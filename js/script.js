d0319f72bcb45f04fbbda563670fe7cf32cb02d9
Francisco Ceballos <ceballosfranciscomartin@gmail.com>
2023-06-02 16:26:18 -0300

    Add & Remove DOM Elements Functionality
    - Also added localStorage for Logged In state.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,66 @@
 "use-strict";
 
-const welcomeSection = document.querySelector("#welcome");
+const logInSection = document.querySelector("#logIn");
+const appSection = document.querySelector("#app");
 
 class App {
   constructor() {
     let name = "";
     let isLoggedIn = false;
     let balance = 0;
+
+    this.#init();
   }
 
-  show(element) {}
+  #getIsLoggedIn() {
+    return localStorage.getItem("isLogged") === "true" || false;
+  }
 
-  update(element) {}
+  #logIn() {
+    localStorage.setItem("isLogged", true);
+    this.#hideAndRemove(logInSection);
+    this.#addAndShow(appSection);
+  }
 
-  hide(element) {}
+  #logOut() {
+    localStorage.setItem("isLogged", false);
+    this.#hideAndRemove(appSection);
+    this.#addAndShow(logInSection);
+  }
 
-  remove(element) {}
+  //   UI Controls
+  #addEle(domElement) {
+    domElement.style.display = "flex";
+  }
+
+  #showEle(domElement) {
+    logInSection.classList.remove("hidden");
+  }
+
+  #updateEle(domElement) {}
+
+  #removeEle(domElement) {
+    domElement.style.display = "none";
+  }
+
+  #hideEle(domElement) {
+    logInSection.classList.add("hidden");
+  }
+
+  #hideAndRemove(domElement) {
+    this.#hideEle(domElement);
+    this.#removeEle(domElement);
+  }
+
+  #addAndShow(domElement) {
+    this.#addEle(domElement);
+    this.#showEle(domElement);
+  }
+
+  #init() {
+    const isLoggedIn = this.#getIsLoggedIn();
+    isLoggedIn ? this.#logIn() : this.#logOut();
+  }
 }
 
 const app = new App();
