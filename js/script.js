e36a3bd7bdc32661300013edc810de3c0511026f
Francisco Ceballos <ceballosfranciscomartin@gmail.com>
2023-06-05 19:07:42 -0300

    New Dolar Value UI Section
    - Also improved responsive design.

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ const logInErrorText = document.querySelector(".welcome__error-text");
 
 const appSection = document.querySelector("#app");
 
+const oficialDolarText = document.querySelector("#dolar-oficial");
+const blueDolarText = document.querySelector("#dolar-blue");
+
 const users = [
   {
     name: "fran",
@@ -27,6 +30,20 @@ class App {
   // User Data Methods
   #userExists(username) {
     return users.filter((user) => user.name === username).length > 0;
+  }
+
+  // Dolar Methods
+  async #fetchDolar() {
+    const response = await fetch("https://api.bluelytics.com.ar/v2/latest");
+    const dolarData = await response.json();
+    return dolarData;
+  }
+
+  async #showDolar() {
+    const dolarValue = await this.#fetchDolar();
+    console.log(dolarValue);
+    oficialDolarText.innerHTML = `Buy: ${dolarValue.oficial.value_buy} - Sell: ${dolarValue.oficial.value_sell}`;
+    blueDolarText.innerHTML = `Buy: ${dolarValue.blue.value_buy} - Sell: ${dolarValue.blue.value_sell}`;
   }
 
   // LogIn Methods
@@ -61,7 +78,7 @@ class App {
     logInSection.classList.remove("hidden");
   }
 
-  #updateEle(domElement) {}
+  #updateUI(domElement) {}
 
   #removeEle(domElement) {
     domElement.style.display = "none";
@@ -97,11 +114,11 @@ class App {
   }
 
   //   UNIT
-
   #init() {
     const isLoggedIn = this.#getIsLoggedIn();
     isLoggedIn ? this.#showAppView() : this.#showLogInView();
     this.#addEventListeners();
+    this.#showDolar();
   }
 
   #addEventListeners() {
