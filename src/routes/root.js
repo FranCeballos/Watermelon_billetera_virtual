b13dbf7f9087ac1724507ca6fb8ca4c9f5134eb4
Francisco Ceballos <ceballosfranciscomartin@gmail.com>
2023-08-04 20:24:23 -0300

    Feat. RootPage Animation

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,5 +1,40 @@
+import { motion } from "framer-motion";
+import classes from "./root.module.css";
+import AnimatedLogo from "../components/UI/AnimatedLogo";
+
+const buttonsVariant = {
+  hidden: {
+    opacity: 0,
+  },
+  show: { opacity: 1, transition: { delay: 4.5, duration: 1, type: "spring" } },
+  exit: {},
+};
+
 const Root = () => {
-  return <div>Watermelon App</div>;
+  return (
+    <motion.div initial="hidden" animate="show" className={classes.container}>
+      <motion.div
+        initial={{ scale: 1.5 }}
+        animate={{ scale: 1 }}
+        transition={{
+          delay: 3,
+          duration: 5,
+          type: "spring",
+          when: "beforeChildren",
+        }}
+        className={classes["logo-box"]}
+      >
+        <AnimatedLogo className={classes["logo"]} />
+      </motion.div>
+      <motion.div
+        variants={buttonsVariant}
+        className={classes["options-container"]}
+      >
+        <button className={classes["btn__primary"]}>Sign In</button>
+        <button className={classes["btn__secondary"]}>Create account</button>
+      </motion.div>
+    </motion.div>
+  );
 };
 
 export default Root;
