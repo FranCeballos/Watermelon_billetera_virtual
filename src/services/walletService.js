85aeb86f387e2359025ce08bd4964864807f7581
Francisco Ceballos <ceballosfranciscomartin@gmail.com>
2023-08-28 13:26:22 -0300

    Feat. Add Token To Server Requests

--- src/services/walletService.js
+++ src/services/walletService.js
@@ -2,7 +2,15 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const walletApi = createApi({
   reducerPath: "walletApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080/app/" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:8080/app/",
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().user.value.token;
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+    },
+  }),
   endpoints: (builder) => ({
     getBalanceAndMovements: builder.query({
       query: (token, _id) => ({
